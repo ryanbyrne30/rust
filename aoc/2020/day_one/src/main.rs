141234dc9910f2df315be6c32e07e24471f1b53c
ryanbyrne30 <ryanjbyrne30@gmail.com>
2023-04-25 12:35:52 -0700

    Solution to day 1 pt 2

--- aoc/2020/day_one/src/main.rs
+++ aoc/2020/day_one/src/main.rs
@@ -6,23 +6,28 @@ pub mod algs;
 const TARGET_SUM: i32 = 2020;
 
 fn main() {
-    let file_path = String::from("src/input.txt");
+	let file_path = String::from("src/input.txt");
 
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read file");
-    let inputs: Vec<&str> = contents.split('\n').collect::<Vec<&str>>();
+	let contents = fs::read_to_string(file_path).expect("Should have been able to read file");
+	let inputs: Vec<&str> = contents.split('\n').collect::<Vec<&str>>();
 
-    let mut numbers: Vec<i32> = vec![];
+	let mut numbers: Vec<i32> = vec![];
 
-    for input in inputs {
-        let n: i32 = input.trim().parse().expect("Not a number");
-        numbers.push(n)
-    }
+	for input in inputs {
+		let n: i32 = input.trim().parse().expect("Not a number");
+		numbers.push(n)
+	}
 
-    sort::qsort(&mut numbers);
+	sort::qsort(&mut numbers);
+	part_one(&numbers);
 
-    let (a, b) = find_sum(&numbers);
+	let sol2 = part_two(&numbers);
+	println!("Solution to part 2 is: {}", sol2);
+}
 
-    println!("{}, {} => {}", a, b, a * b);
+fn part_one(v_sorted: &Vec<i32>) {
+	let (a, b) = find_sum(v_sorted);
+	println!("Solution to part one is: Product of {} and {} is {}", a, b, a * b);
 }
 
 
@@ -32,17 +37,44 @@ fn find_sum(v_sorted: &Vec<i32>) -> (i32, i32) {
     let mut sum: i32 = v_sorted[left] + v_sorted[right];
 
     while sum != TARGET_SUM && left < right {
-        if sum < TARGET_SUM {
-            left += 1
-        } else if sum > TARGET_SUM {
-            right -= 1
-        }
-        sum = v_sorted[left] + v_sorted[right]
+			if sum < TARGET_SUM {
+				left += 1
+			} else if sum > TARGET_SUM {
+				right -= 1
+			}
+			sum = v_sorted[left] + v_sorted[right]
     }
 
     if left >= right {
-        panic!("No solution")
+			panic!("No solution")
     }
 
     (v_sorted[left], v_sorted[right])
+}
+
+fn part_two(v_sorted: &Vec<i32>) -> i32 {
+	let len = v_sorted.len();
+
+	let mut i: usize = 0;
+	while i < len {
+		let mut j: usize = i + 1;
+		while j < len {
+			let mut k: usize = j + 1;
+			while k < len {
+				let sum = v_sorted[i] + v_sorted[j] + v_sorted[k];
+
+				if sum == TARGET_SUM {
+					return v_sorted[i] * v_sorted[j] * v_sorted[k];
+				}
+
+				k += 1;
+			}
+
+			j += 1;
+		}
+
+		i += 1;
+	}
+
+	panic!("No solution")
 }
