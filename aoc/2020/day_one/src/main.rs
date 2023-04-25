41c63b48e723ecda259133a5957227bc0ad4fdbe
ryanbyrne30 <ryanjbyrne30@gmail.com>
2023-04-25 11:41:00 -0700

    Solved day_one pt 1

--- aoc/2020/day_one/src/main.rs
+++ aoc/2020/day_one/src/main.rs
@@ -3,6 +3,8 @@ use crate::algs::sort;
 
 pub mod algs;
 
+const TARGET_SUM: i32 = 2020;
+
 fn main() {
     let file_path = String::from("src/input.txt");
 
@@ -18,7 +20,29 @@ fn main() {
 
     sort::qsort(&mut numbers);
 
-    for number in numbers {
-        println!("{}", number);
+    let (a, b) = find_sum(&numbers);
+
+    println!("{}, {} => {}", a, b, a * b);
+}
+
+
+fn find_sum(v_sorted: &Vec<i32>) -> (i32, i32) {
+    let mut left: usize = 0;
+    let mut right: usize = v_sorted.len() - 1;
+    let mut sum: i32 = v_sorted[left] + v_sorted[right];
+
+    while sum != TARGET_SUM && left < right {
+        if sum < TARGET_SUM {
+            left += 1
+        } else if sum > TARGET_SUM {
+            right -= 1
+        }
+        sum = v_sorted[left] + v_sorted[right]
     }
+
+    if left >= right {
+        panic!("No solution")
+    }
+
+    (v_sorted[left], v_sorted[right])
 }
