33aaf8feed8b4ad2f81e8f7631f71fdddf9e3894
ryanbyrne30 <ryanjbyrne30@gmail.com>
2023-04-26 13:41:28 -0700

    Longest subsequence uses indices

--- algorithms/src/algs/dyn_prog/longest_subsequence.rs
+++ algorithms/src/algs/dyn_prog/longest_subsequence.rs
@@ -1,20 +1,17 @@
 pub fn solve(s1: &str, s2: &str) -> String {
-  helper(s1.as_bytes(), s2.as_bytes())
+  helper(s1.as_bytes(), s2.as_bytes(), s1.len(), s2.len())
 }
 
-fn helper(s1: &[u8], s2: &[u8]) -> String {
-  let l1 = s1.len();
-  let l2 = s2.len();
-
-  if l1 == 0 || l2 == 0 {
+fn helper(s1: &[u8], s2: &[u8], m: usize, n: usize) -> String {
+  if m == 0 || n == 0 {
     String::from("")
-  } else if s1[l1 - 1] == s2[l2 - 1] {
-    let mut result = helper(&s1[..l1-1], &s2[..l2-1]);
-    result.push_str(&String::from(char::from(s1[l1 - 1])));
+  } else if s1[m - 1] == s2[n - 1] {
+    let mut result = helper(s1, s2, m - 1, n - 1);
+    result.push_str(&String::from(char::from(s1[m - 1])));
     result
   } else {
-    let a = helper(&s1[..l1-1], s2);
-    let b = helper(s1, &s2[..l2-1]);
+    let a = helper(s1, s2, m - 1, n);
+    let b = helper(s1, s2, m, n - 1);
     if a.len() > b.len() {
       a
     } else {
